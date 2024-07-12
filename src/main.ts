0c35204313db7115e8a8002810aa6477d0971c75
Josue Ordonez <razuordonez@gmail.com>
2024-07-12 11:01:25 -0500

    main log

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_PORT);
+  console.log(process.env.DB_NAME);
+  console.log(process.env.DB_USERNAME);
+  console.log(process.env.DB_PASSWORD);
+
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
