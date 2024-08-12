477a1a12bb34917e0bf34c29c9cd7018dc626746
Josue Ordonez <razuordonez@gmail.com>
2024-08-12 16:48:27 -0500

    history && appointment implemented

--- src/appointment/appointment.module.ts
+++ src/appointment/appointment.module.ts
@@ -8,15 +8,9 @@ import { AppointmentController } from './appointment.controller';
 
 import { AuthModule } from 'src/auth/auth.module';
 import { PatientModule } from 'src/patient/patient.module';
-import { MedicInfoModule } from 'src/medic-info/medic-info.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Appointment]),
-    AuthModule,
-    PatientModule,
-    MedicInfoModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Appointment]), AuthModule, PatientModule],
   controllers: [AppointmentController],
   providers: [AppointmentService],
 })
